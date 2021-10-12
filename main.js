0272a4c8573598f43ffec5a0b006eb18ff3d4a84
quineshabaldwin13 <baldwinquinesha@gmailcom>
2021-10-12 12:58:16 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,54 @@
 // │ Your code here! │
 // └─────────────────┘
 
+function createUser(fName, lName) {
+    const user = {
+    firstName: fName,
+    lastName: lName
+};
+return user;
+ }
 
+//Create Set Age
+
+function setAge(userOne, ageOne) {
+    userOne['age'] = ageOne;
+    return userOne;
+}
+
+//Create increment Age
+
+function incrementAge(user) {
+    user.age += 1;
+    return user;
+}
+
+//Create fix car
+
+function fixCar(car) {
+    car.needsMaitenance = false
+    return car;
+}
+
+//Create add grades
+
+function addGrades(student, newGrades) {
+    for (let i = 0; i < newGrades.length; i++) {
+        const newGrade = newGrades[i];
+        student.grades.push(newGrade);
+    }
+    return student;
+}
+
+//Get Data type
+
+function getDataType(car) {
+    make: 'Ford',
+    model: 'Mustang',
+    year: '1969',
+    needsMaitenance: false
+    return car;
+}
 
 
 
